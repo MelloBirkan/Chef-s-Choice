4c5c272b23a582c4c673adba6e9fc3749d0541d9
Mello Birkan <mgbirkan@icloud.com>
2023-12-22 01:03:52 -0300

    Initial Commit

---
+++ Chef'sChoice/Chef_sChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_sChoiceApp.swift
+//  Chef'sChoice
+//
+//  Created by Marcello Gonzatto Birkan on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Chef_sChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chef'sChoice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chef'sChoice
+//
+//  Created by Marcello Gonzatto Birkan on 22/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
